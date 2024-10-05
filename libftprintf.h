e04c48e0c4bf69a6ecde583053666523d2984e45
Kaï Doh <github@kaidoh.ch>
2024-10-05 14:47:24 +0200

    beginning of ft_printf

---
+++ libftprintf.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ktiomico <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/05 14:03:09 by ktiomico          #+#    #+#             */
+/*   Updated: 2024/10/05 14:04:44 by ktiomico         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+
+#endif
